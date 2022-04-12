c4e0f1b38945aa2d020f2ef699f8b87aaba09b47
johnny <john_newcreek@protonmail.com>
2022-04-12 16:08:08 +0300

    added mouse hover to change color of squares, gaining ability to paint

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 
 const container = document.querySelector('.container');
 
-for(i=0; i<=(16*16); i++){
-    let gridSquare = document.createElement('div');
+for(i=1; i<=(16*16); i++){
+    const gridSquare = document.createElement('div');
     gridSquare.className = gridSquare;
-    gridSquare.setAttribute('class', 'gridSquare')
+    gridSquare.setAttribute('class', 'gridSquare');
     gridSquare.textContent =[i];
     container.appendChild(gridSquare);
 };
+
+let gridSq = document.querySelectorAll('.gridSquare');
+
+gridSq.forEach((square) => {
+    square.addEventListener('mouseenter', changeColor);
+});
+
+function changeColor(e) {
+    e.target.style.backgroundColor = "black";
+    setTimeout()
+};
+
+setTimeout(function(e) {
+    e.target.style.backgroundColor = "";
+  }, 500);
