0ea4b479075590f1f141f84cdc6ae8107010e256
johnny <john_newcreek@protonmail.com>
2022-04-12 21:52:44 +0300

    put the square(div) generator loop and color change function inside a function named generateGrid(), added reset() to reset the grid with button which gives prompt to change size of grid

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+
 let gridSize = 16;
 const container = document.querySelector('.container');
+
+function generateGrid(){
+    for(i=1; i<=(gridSize*gridSize); i++){
+    const gridSquare = document.createElement('div');
+    // gridSquare.className = gridSquare;
+    gridSquare.setAttribute('class', 'gridSquare');
+    gridSquare.textContent =[i];
+    container.appendChild(gridSquare);
+};
+
 const gridSq = document.querySelectorAll('.gridSquare');
 console.log(gridSq)
-
-
-
 /*******eventlistener for each square********/
 gridSq.forEach((square) => {
     square.addEventListener('mouseenter', changeColor);
@@ -20,22 +28,21 @@ function changeColor(e) {
         e.target.style.backgroundColor = "";
     }, 1500);
 };
+};
 
-    for(i=1; i<=(gridSize*gridSize); i++){
-        const gridSquare = document.createElement('div');
-        // gridSquare.className = gridSquare;
-        gridSquare.setAttribute('class', 'gridSquare');
-        gridSquare.textContent =[i];
-        container.appendChild(gridSquare);
-    };
+generateGrid();
 
-// function reset() {
-//     let answer = parseInt(window.prompt("choose a grid size:", "1-100"));
-//     console.log(answer)
-//      if (answer > 100 || answer < 1) {
-//          alert('the number passed in needs to be in the 1-100 range');
-//      } else {
-//         gridSize = answer;
-//         container.style.setProperty('--grid-size', `${answer}`)
-//     }
-// }
+function reset() {
+    let answer = parseInt(window.prompt("choose a grid size:", "1-100"));
+    console.log(answer)
+     if (answer > 100 || answer < 1) {
+         alert('the number passed in needs to be in the 1-100 range');
+     } else {
+        gridSize = answer;
+        container.style.setProperty('--grid-size', `${answer}`)
+        while(container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        generateGrid();
+    }
+}
